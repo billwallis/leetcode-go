f3cedbb5a5be355d38eb47723558331d464562ec
Bill <bill@thewallisfamily.co.uk>
2024-02-08 09:28:04 +0000

    ✨ feat: Add solution for problem 8

---
+++ internal/solutions/008__string_to_integer_atoi.go
@@ -0,0 +1,83 @@
+package solutions
+
+import (
+	"math"
+	"slices"
+	"strconv"
+)
+
+func LeftTrim(s string, c string) string {
+	//if len(c) != 1 {
+	//	panic("c must be a single character")
+	//}
+	for len(s) > 0 && s[0] == c[0] {
+		s = s[1:]
+	}
+	return s
+}
+
+func Within(number int, min int, max int) int {
+	// Validation option 1
+	//if max > min {
+	//	panic("min is greater than max")
+	//}
+
+	// Validation option 2
+	//min, max := math.Min(min, max), math.Max(min, max)
+
+	if number <= min {
+		return min
+	} else if number >= max {
+		return max
+	} else {
+		return number
+	}
+}
+
+// MyAtoi is the eighth LeetCode problem:
+// - https://leetcode.com/problems/string-to-integer-atoi/
+func MyAtoi(s string) int {
+	sign, result := "", ""
+	str := LeftTrim(s, " ")
+	minimum := int(math.Pow(float64(-2), float64(31)))
+	maximum := int(-1 + math.Pow(float64(2), float64(31)))
+
+	if len(str) != 0 {
+		if firstChar := string(str[0]); slices.Contains([]string{"-", "+"}, firstChar) {
+			sign = firstChar
+			str = str[1:]
+		}
+	}
+
+	str = LeftTrim(str, "0")
+	if str == "" {
+		return 0
+	}
+
+	for _, char := range str {
+		// 0 to 9 (integers) == 48 to 57 (bytes)
+		if char >= 48 && char <= 57 {
+			result += string(char)
+		} else {
+			if result == "" {
+				return 0
+			}
+			break
+		}
+	}
+
+	if len(result) > 10 {
+		if sign == "-" {
+			return minimum
+		} else {
+			return maximum
+		}
+	}
+
+	ret, err := strconv.Atoi(sign + result)
+	if err != nil {
+		panic(err)
+	} else {
+		return Within(ret, minimum, maximum)
+	}
+}
---
+++ test/internal/solutions/008__string_to_integer_atoi__test.go
@@ -0,0 +1,73 @@
+package solutions_test
+
+import (
+	"github.com/Bilbottom/leetcode-go/internal/solutions"
+	"testing"
+)
+
+func TestLeftTrim(t *testing.T) {
+	var tests = []struct {
+		s    string
+		c    string
+		want string
+	}{
+		{s: "000000", c: "0", want: ""},
+		{s: "     1", c: " ", want: "1"},
+		{s: "  1   ", c: " ", want: "1   "},
+		{s: "012345", c: "0", want: "12345"},
+	}
+
+	for _, test := range tests {
+		got := solutions.LeftTrim(test.s, test.c)
+		if got != test.want {
+			t.Fatalf("LeftTrim(%s, %s) == %s, want %s", test.s, test.c, got, test.want)
+		}
+	}
+}
+
+func TestWithin(t *testing.T) {
+	var tests = []struct {
+		number int
+		min    int
+		max    int
+		want   int
+	}{
+		{number: 0, min: 0, max: 0, want: 0},
+		{number: 5, min: 0, max: 10, want: 5},
+		{number: -1, min: 0, max: 10, want: 0},
+		{number: 11, min: 0, max: 10, want: 10},
+	}
+
+	for _, test := range tests {
+		got := solutions.Within(test.number, test.min, test.max)
+		if got != test.want {
+			t.Fatalf("Within(%d, %d, %d) == %d, want %d", test.number, test.min, test.max, got, test.want)
+		}
+	}
+}
+
+func TestMyAtoi(t *testing.T) {
+	var tests = []struct {
+		s    string
+		want int
+	}{
+		{s: "42", want: 42},
+		{s: "   -42", want: -42},
+		{s: "4193 with words", want: 4193},
+		{s: "-91283472332", want: -2147483648},
+		{s: "words and 987", want: 0},
+		{s: "", want: 0},
+		{s: "+", want: 0},
+		{s: "    0000000000000   ", want: 0},
+		{s: "   +0 123", want: 0},
+		{s: "20000000000000000000", want: 2147483647},
+		{s: "    +00001 123 and this is a whole bunch of other text", want: 1},
+	}
+
+	for _, test := range tests {
+		got := solutions.MyAtoi(test.s)
+		if got != test.want {
+			t.Fatalf("MyAtoi(%s) == %d, want %d", test.s, got, test.want)
+		}
+	}
+}
