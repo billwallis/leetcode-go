7caed4054a39702ae15eb0728360a6576345c44c
Bill <bill@thewallisfamily.co.uk>
2024-02-21 07:50:13 +0000

    ✨ feat: Add improved solution for problem 24

--- internal/solutions/024__swap_nodes_in_pairs.go
+++ internal/solutions/024__swap_nodes_in_pairs.go
@@ -1,7 +1,8 @@
 package solutions
 
-// swapPairs is the twenty-fourth LeetCode problem:
-// - https://leetcode.com/problems/swap-nodes-in-pairs/
+// This the first solution, before realising that this swaps _values_
+// when the problem asked to swap _nodes_.
+/*
 func swapPairs(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
@@ -14,6 +15,21 @@ func swapPairs(head *ListNode) *ListNode {
 			Next: swapPairs(head.Next.Next),
 		},
 	}
+}
+*/
+
+// swapPairs is the twenty-fourth LeetCode problem:
+// - https://leetcode.com/problems/swap-nodes-in-pairs/
+func swapPairs(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	swap := head.Next
+	head.Next = swapPairs(head.Next.Next)
+	swap.Next = head
+
+	return swap
 }
 
 var SwapPairs = swapPairs /* For testing */
