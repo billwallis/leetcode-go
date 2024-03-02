62221586c17f117b0e978757d76096c41d4099c9
Bill <bill@thewallisfamily.co.uk>
2024-03-02 08:43:22 +0000

    ✨ feat: Add files for problem 37

---
+++ internal/solutions/037__sudoku_solver.go
@@ -0,0 +1,9 @@
+package solutions
+
+// solveSudoku is the thirty-seventh LeetCode problem:
+// - https://leetcode.com/problems/sudoku-solver/
+func solveSudoku(board [][]byte) {
+	print(board)
+}
+
+var SolveSudoku = solveSudoku /* For testing */
