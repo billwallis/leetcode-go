792773cdcc93bf0946ce0ab9766ce53d35d7be28
Bill <bill@thewallisfamily.co.uk>
2024-03-03 08:50:36 +0000

    ✨ feat: Add files for problem 39

---
+++ internal/solutions/039__combination_sum.go
@@ -0,0 +1,10 @@
+package solutions
+
+// combinationSum is the thirty-ninth LeetCode problem:
+// - https://leetcode.com/problems/combination-sum/
+func combinationSum(candidates []int, target int) [][]int {
+	print(candidates, target)
+	return [][]int{}
+}
+
+var CombinationSum = combinationSum /* For testing */
---
+++ test/internal/solutions/039__combination_sum__test.go
@@ -0,0 +1,28 @@
+package solutions_test
+
+import (
+	"github.com/Bilbottom/leetcode-go/internal/solutions"
+	"reflect"
+	"testing"
+)
+
+func TestCombinationSum(t *testing.T) {
+	t.Skip("Not implemented yet...")
+
+	var tests = []struct {
+		candidates []int
+		target     int
+		want       [][]int
+	}{
+		{candidates: []int{2}, target: 1, want: [][]int{}},
+		{candidates: []int{2, 3, 6, 7}, target: 7, want: [][]int{{2, 2, 3}, {7}}},
+		{candidates: []int{2, 3, 5}, target: 8, want: [][]int{{2, 2, 2, 2}, {2, 3, 3}, {3, 5}}},
+	}
+
+	for _, test := range tests {
+		got := solutions.CombinationSum(test.candidates, test.target)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Fatalf("CombinationSum(%v, %d) == %v, want %v", test.candidates, test.target, got, test.want)
+		}
+	}
+}
